b04faf523235ec0c5bd0dd1753dc61ad1c445793
Joha Kim <johakim9936@gmail.com>
2020-07-01 16:55:06 -0400

    delete and update documents

--- app.js
+++ app.js
@@ -33,19 +33,46 @@ const apple = new Fruit({ name: "Apple", color: "Red", rating: 8 });
 const kiwi = new Fruit({ name: "Kiwi", color: "Green", rating: 8 });
 const banana = new Fruit({ name: "Banana", color: "Yellow", rating: 8 });
 const mango = new Fruit({ name: "Mango", color: "Orange", rating: 8 });
+const dragonfruit = new Fruit({
+  name: "Dragonfruit",
+  color: "Purple",
+  rating: 8,
+});
+
+// validation check
+// dragonfruit.save();
 
 // saves the document into a collection
 // apple.save();
 // Fruit.insertMany([apple, kiwi, banana, mango], function (error, docs) {});
 
+// deletes one document in a collection
+Fruit.deleteOne({ name: "Kiwi" }, function (err) {
+  if (err) {
+    console.log("Not able to delete document.");
+  } else {
+    console.log("Successfully deleted document.");
+  }
+});
+
+// update accepts filter to find the doc you want to edit and the changes you want to make to it
+Fruit.updateOne({ name: "Apple" }, { name: "Fuji Apple" }, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully updated document.");
+  }
+});
+
+// gets all the documents in a collection
 Fruit.find(function (err, fruits) {
   if (err) {
     console.log(err);
   } else {
-    mongoose.connection.close();
     fruits.forEach((fruit) => {
-      console.log(fruit.name);
+      console.log(fruit);
     });
+    mongoose.connection.close(); 
   }
 });
 
