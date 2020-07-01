e4a4f6b92c8ca02b37b5b95befde34502c137644
Joha Kim <johakim9936@gmail.com>
2020-07-01 15:56:15 -0400

    iterate through documents in fruit collection

--- app.js
+++ app.js
@@ -19,5 +19,31 @@ const Fruit = mongoose.model("Fruit", fruitSchema);
 // create new document to go into a collection
 const fruit = new Fruit({ name: "Apple", color: "Red", rating: 8 });
 
-// saves the document into a collection 
-fruit.save(); 
+const kiwi = new Fruit({ name: "Kiwi", color: "Green", rating: 8 });
+const banana = new Fruit({ name: "Banana", color: "Yellow", rating: 8 });
+const mango = new Fruit({ name: "Mango", color: "Orange", rating: 8 });
+
+// saves the document into a collection
+// fruit.save();
+// Fruit.insertMany([kiwi, banana, mango], function (error, docs) {});
+
+Fruit.find(function (err, fruits) {
+  if (err) {
+    console.log(err);
+  } else {
+    fruits.forEach((fruit) => {
+      console.log(fruit.name);
+    });
+  }
+});
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({ name: "Bob", age: 30 });
+
+// person.save();
